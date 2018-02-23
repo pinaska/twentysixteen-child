86e536b7fef317345a473eaf0c1d1937354dd4cb
Kate Jeziorska <k_jeziorska@yahoo.com>
2018-02-22 22:04:51 -0800

    changes to full-width.php, it's working!

--- full-width.php
+++ full-width.php
@@ -6,13 +6,23 @@ Template Name: Full-Width
 get_header(); ?>
 <div id="primary" class="page-template-nosidebar-page page-template-fullwidth-page">
 	<main id="main" class="site-main page-template-fullwidth-page" role="main">
-<?php
-$latest_blog_posts = new WP_Query( array( 'posts_per_page' => 3 ) );
 
-if ( $latest_blog_posts->have_posts() ) : while ( $latest_blog_posts->have_posts() ) : $latest_blog_posts->the_post();
-    // Loop output goes here
-endwhile; endif;
-		?>
+<?php
+//$latest_blog_posts = new WP_Query( array( 'posts_per_page' => 3 ) );
+// Start the loop.
+while ( have_posts() ) : the_post();
+
+	// Include the page content template.
+	get_template_part( 'template-parts/content', 'page' );
+
+	// If comments are open or we have at least one comment, load up the comment template.
+	if ( comments_open() || get_comments_number() ) {
+		comments_template();
+	}
+
+	// End of the loop.
+endwhile;
+?>
 
 	</main><!-- .site-main -->
 
